8e7fe61f4f1fcb1cfc1828e23508d4a67f64a78f
Jehmiah Pittman <jehmiah@ucsb.edu>
2025-11-05 06:25:54 -0800

    Added Placeholders & modifed App, AppNavbar to include HelpRequest

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -71,6 +71,10 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/helprequest">
+                  Help Request
+                  </Nav.Link>
+
                 </>
               ) : (
                 <></>
