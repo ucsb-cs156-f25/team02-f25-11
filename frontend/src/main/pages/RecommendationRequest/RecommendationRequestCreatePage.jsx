f3022f860da0da7e6f82f5072b5a059994d3ccbc
Sagnik Biswas <sagnikbiswas@ucsb.edu>
2025-11-05 00:55:16 -0800

    added create story

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
@@ -1,11 +1,48 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequests/RecommendationRequestForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function RecommendationRequestCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (req) => ({
+    url: "/api/recommendationrequests/post",
+    method: "POST",
+    params: {
+      requesterEmail: req.requesterEmail,
+      professorEmail: req.professorEmail,
+      explanation: req.explanation,
+      dateRequested: req.dateRequested,
+      dateNeeded: req.dateNeeded,
+      done: req.done,
+    },
+  });
+
+  const onSuccess = (saved) => {
+    toast(
+      `New Recommendation Request Created - id: ${saved.id} requester: ${saved.requesterEmail}`,
+    );
+  };
+
+  const mutation = useBackendMutation(objectToAxiosParams, { onSuccess }, [
+    "/api/recommendationrequests/all",
+  ]);
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequest" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Recommendation Request</h1>
+        <RecommendationRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
